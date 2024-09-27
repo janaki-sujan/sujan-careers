aba1593b4fcb5e2f95e93698ed922681c07ee472
janaki-sujan <166840189+janaki-sujan@users.noreply.github.com>
2024-09-27 15:52:18 +0000

    added  a dynamic url to templates

--- app.py
+++ app.py
@@ -1,11 +1,62 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, jsonify 
 
 app = Flask(__name__)
+JOBS = [{
+    'id': 1,
+    'title': 'Data Analyst',
+    'location': 'Bengaluru, India',
+    'salary': 10000
+}, {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi,India',
+    'salary': 25000
+}, {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Remote',
+    'salary': 15000
+}, {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'San Francisco, USA',
+    'salary': 20000
+}, {
+    'id': 5,
+    'title': 'Software Engineer',
+    'location': 'San Francisco, USA',
+    'salary': 50000
+}, {
+    'id': 6,
+    'title': 'Machine Learning Engineer',
+    'location': 'San Francisco, USA',
+    'salary': 80000
+}, {
+    'id': 7,
+    'title': 'Data Engineer',
+    'location': 'San Francisco, USA',
+    'salary': 400000
+}, {
+    'id': 8,
+    'title': 'Product Manager',
+    'location': 'San Francisco, USA',
+    'salary': 60000
+}, {
+    'id': 9,
+    'title': 'Business Analyst',
+    'location': 'San Francisco, USA',
+    'salary': 33000
+}]
+
+
+@app.route('/jobs')
+def list_jobs():
+    return jsonify(JOBS)
 
 
 @app.route('/')
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
 
 print(__name__)
